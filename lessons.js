7118c13a678e4d97b39dc3866ce071aa7daab5af
bhavneetkaurraheja <banirahejax@gmail.com>
2023-11-09 14:41:14 +0000

    VueAfterClassApp

---
+++ lessons.js
@@ -0,0 +1,113 @@
+var lesson = [ 
+    
+        {
+          "id": 1,
+          "name": "Math Class",
+          "location": "Ilford",
+          "price": 30,
+          "image": "./Images/math1.jpeg",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 2,
+          "name": "English Class",
+          "location": "Manor Park",
+          "price": 35,
+          "image": "./Images/english1.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 3,
+          "name": "Science Class",
+          "location": "Romford",
+          "price": 35,
+          "image": "./Images/science1.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 4,
+          "name": "Social Science Class",
+          "location": "Brentwood",
+          "price": 25,
+          "image": "./Images/social1.gif",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 5,
+          "name": "Dance Class",
+          "location": "Liverpool Street",
+          "price": 45,
+          "image": "./Images/dance.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 6,
+          "name": "Drawing Class",
+          "location": "Seven Kings",
+          "price": 30,
+          "image": "./Images/drawing1.jpg",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 7,
+          "name": "Music Class",
+          "location": "Shenfield",
+          "price": 45,
+          "image": "./Images/music.jpg",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 8,
+          "name": "Drama Class",
+          "location": "Manchester",
+          "price": 45,
+          "image": "./Images/drama1.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 9,
+          "name": "Football Class",
+          "location": "Woodford",
+          "price": 50,
+          "image": "./Images/football.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 10,
+          "name": "Karate Class",
+          "location": "BrentCross",
+          "price": 40,
+          "image": "./Images/karate.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 11,
+          "name": "Badminton Class",
+          "location": "Moorgate",
+          "price": 35,
+          "image": "./Images/badminton.png",
+          "availableInventory": 5,
+          "message": ""
+        },
+        {
+          "id": 12,
+          "name": "Gymnastic Class",
+          "location": "Hendon",
+          "price": 35,
+          "image": "./Images/gymnastic.png",
+          "availableInventory": 5,
+          "message": ""
+        }
+      ]
+      
+
