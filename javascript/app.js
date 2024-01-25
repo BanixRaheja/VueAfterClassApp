e1891eb2e573d217c85e411b2e6308a988b6ee8f
bhavneetkaurraheja <banirahejax@gmail.com>
2024-01-25 21:56:22 +0000

    updated

--- javascript/app.js
+++ javascript/app.js
@@ -1,34 +1,40 @@
-let webstore = new Vue({
+new Vue({
   el: "#app",
+
   data: {
-    sitename: " After Classes Depot",
-    showsubject: true,
     loading: false,
     error: null,
     url: "https://webcoursework.eu-north-1.elasticbeanstalk.com",
-    sortAttribute: "Price",
-    sortOrder: "asc", //Default
-    sortingMessage: "Items currently sorted by Price in Ascending order",
     searchText: "",
-    showMessage: false,
-    isFormValid: false,
-    order: {
-      firstName: "",
-      PhoneNumber: "",
-      firstNameValid: true,
-      phoneNumberValid: true,
+    sortBy: "subject",
+    orderBy: "asc",
+    sortOptions: ["subject", "location", "price", "availability"],
+    orders: [
+      {
+        text: "Ascending",
+        value: "asc",
+      },
+      {
+        text: "Descending",
+        value: "desc",
+      },
+    ],
+    cart: [],
+    isCartDisplaying: false,
+    checkedOut: false,
+    checkoutForm: {
+      name: {
+        value: "",
+        error: "",
+      },
+      phone: {
+        value: "",
+        error: "",
+      },
     },
-
-    errorMessages: {
-      firstName: "",
-      PhoneNumber: "",
-    },
-    firstNameValid: true,
-    phoneNumberValid: true,
-
-    subject: [],
-    cart: [], //array to store items in shopping cart
+    lessons: [],
   },
+
   methods: {
     async getLessons() {
       try {
@@ -62,17 +68,17 @@ let webstore = new Vue({
         this.loading = false;
       }
     },
-    async updateLesson({ subject_id, availableInventory }) {
+    async updateLesson({ lesson_id, spaces }) {
       try {
         this.loading = true;
 
-        const url = `${this.url}/lessons/${subject_id}`;
+        const url = `${this.url}/lessons/${lesson_id}`;
 
         const response = await fetch(url, {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            availableInventory: availableInventory,
+            spaces: spaces,
           }),
         });
       } catch (error) {
@@ -81,246 +87,216 @@ let webstore = new Vue({
         this.loading = false;
       }
     },
-    cartCount(id) {
-      let count = 0;
-      for (let i = 0; i < this.cart, length; i++) {
-        if (this.cart[i] === id) {
-          count++;
-        }
-      }
-      return count;
-    },
-    addToCart: function (subject) {
-      if (subject.availableInventory > 0) {
-        this.cart.push(subject.id);
-        subject.availableInventory--;
+    updateLessonSpaces(type, _id) {
+      switch (type) {
+        case "decrease":
+          this.lessons = this.lessons.map((item) => {
+            if (item._id === _id && item.spaces > 0)
+              return { ...item, spaces: --item.spaces };
 
-        //update itemsLeft property
-        subject.itemsLeft = subject.availableInventory;
-      }
-    },
-
-    returnToMainScreen() {
-      this.showsubject = true; // Set showsubject to true to display the main screen
-      this.cart = []; // Clear the cart
-      // this.resetOrder(); // Reset the order
-    },
-    updateSortingMessage() {
-      const criteria =
-        this.sortAttribute.charAt(0).toUpperCase() + this.sortAttribute.slice(1);
-      const order = this.sortOrder === "asc" ? "Ascending" : "Descending";
-      this.sortingMessage = `Items currently sorted by ${criteria} in ${order} order`;
-    },
-
-    showCheckout: function () {
-      if (this.cart.length === 0) {
-        console.log("Show checkout clicked with empty cart");
-        this.showMessage = true;
-      } else {
-        console.log("Show checkout clicked with items in cart");
-        this.showsubject = !this.showsubject;
-      }
-    },
-    removeSubjectFromCart(subjectId) {
-      const index = this.cart.findIndex((itemId) => itemId === subjectId);
-      if (index !== -1) {
-        this.cart.splice(index, 1);
-        const subject = this.subject.find((item) => item.id === subjectId);
-        if (subject) {
-          subject.availableInventory++;
-        }
-        if (this.cart.length === 0) {
-          this.showsubject = true;
-        }
-      }
-    },
-    addToCartFromCheckout(subjectId) {
-      const subject = this.subject.find((item) => item.id === subjectId);
-      if (subject && subject.availableInventory > 0) {
-        this.cart.push(subject.id);
-        subject.availableInventory--;
-      }
-    },
-
-    validateFirstName() {
-      if (!/^[A-Za-z]+$/.test(this.order.firstName)) {
-        this.errorMessages.firstName = "Please enter letters only";
-        this.firstNameValid = false;
-      } else {
-        this.errorMessages.firstName = "";
-        this.firstNameValid = true;
-      }
-      this.updateFormValidation();
-    },
-
-    validatePhoneNumber() {
-      if (!/^((\+44)|(0)) ?\d{4} ?\d{6}$/.test(this.order.PhoneNumber)) {
-        this.errorMessages.PhoneNumber =
-          "Please enter phone number in UK Format, starts with 0 or +44.";
-        this.phoneNumberValid = false;
-      } else {
-        this.errorMessages.PhoneNumber = "";
-        this.phoneNumberValid = true;
-      }
-      this.updateFormValidation();
-    },
-
-    updateFormValidation() {
-      this.isFormValid =
-        this.firstNameValid &&
-        this.phoneNumberValid &&
-        this.order.firstName &&
-        this.order.PhoneNumber;
-    },
-
-
-    submitForm() {
-      if (this.isFormValid) {
-        alert("Order Submitted!");
-        // Reset the cart and order form data
-        this.cart.forEach(async (item) => {
-          await this.createNewOrder({
-            firstName: this.order.firstName,
-            PhoneNumber: this.order.PhoneNumber,
-            subject_id: subject.subjectId,
-            availableInventory: availableInventory
+            return item;
           });
+          break;
 
-          await this.updateLesson({
-            subject_id : subject.subjectId,
-            availableInventory: subject.availableInventory,
+        case "reset":
+          this.lessons = this.lessons.map((item) => {
+            if (item._id === _id) return { ...item, spaces: 5 };
+
+            return item;
           });
+          break;
+
+        default:
+          break;
+      }
+    },
+    isItemInCart(_id) {
+      return !!this.cart.find((item) => item._id === _id);
+    },
+    addToCart(_id) {
+      const lesson = this.lessons.find((lesson) => lesson._id === _id);
+
+      if (!this.isItemInCart(lesson._id)) {
+        this.cart.push({
+          ...lesson,
+          spaces: 1,
+        });
+      } else {
+        if (lesson.spaces > 0) {
+          this.cart = this.cart.map((item) => {
+            if (item._id === _id)
+              return { ...item, spaces: ++item.spaces };
+            return item;
+          });
+        }
+      }
+
+      this.updateLessonSpaces("decrease", lesson._id);
+    },
+    removeFromCart(_id) {
+      // get index of cart item
+      const index = this.cart.findIndex((item) => item._id === _id);
+
+      // remove item from cart
+      this.cart.splice(index, 1);
+
+      // update lesson spaces
+      this.updateLessonSpaces("reset", _id);
+
+      // toggle cart display if no item is in cart
+      if (!this.cart.length) this.toggleCartDisplay();
+    },
+    toggleCartDisplay() {
+      this.isCartDisplaying = !this.isCartDisplaying;
+    },
+    checkout() {
+      this.cart.forEach(async (item) => {
+        await this.createNewOrder({
+          name: this.checkoutForm.name.value,
+          phone: this.checkoutForm.phone.value,
+          lesson_id: item._id,
+          spaces: item.spaces,
         });
 
-        this.showsubject = true;
-        for (let i = 0; i < this.subject.length; i++) {
-          this.subject[i].availableInventory = 5;
-        }
-      } else {
-        alert("Please fill in all fields correctly.");
-      }
+        await this.updateLesson({
+          lesson_id: item._id,
+          spaces: item.spaces,
+        });
+      });
+
+      this.checkedOut = true;
+
+      this.toggleCartDisplay();
+
+      this.cart = [];
+
+      Object.keys(this.checkoutForm).every(
+        (key) => (this.checkoutForm[key].value = "")
+      );
+
+      setTimeout(() => {
+        this.checkedOut = false;
+      }, 3000);
     },
   },
 
   computed: {
-    cartItemCount: function () {
-      console.log("works");
-      return this.cart.length;
+    cartLength() {
+      if (this.cart.length > 0)
+        return this.cart.reduce((total, item) => total + item.spaces, 0);
+      return 0;
     },
+    filteredLessons() {
+      if (this.lessons.length <= 0) return [];
 
-    canAddToCart() {
-      return (subject) => {
-        // return subject.availableInventory > 0;
-        return subject.availableInventory > this.cartCount(subject.id);
-      };
-    },
-    sortedSubjects: function () {
-      const subjects = this.subject.filter(
-        (subject) =>
-          subject.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
-          subject.location.toLowerCase().includes(this.searchText.toLowerCase())
-      );
+      const lessons = this.lessons;
 
-      if (this.sortOrder === "asc") {
-        // Ascending order
-        switch (this.sortAttribute) {
-          case "name":
-            return subjects.sort((a, b) => {
-              if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
-              if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+      if (this.orderBy === "asc") {
+        // ascending
+        switch (this.sortBy) {
+          case "subject":
+            return lessons.sort((a, b) => {
+              if (a.subject.toLowerCase() > b.subject.toLowerCase())
+                return 1;
+              if (a.subject.toLowerCase() < b.subject.toLowerCase())
+                return -1;
               return 0;
             });
           case "location":
-            return subjects.sort((a, b) => {
-              if (a.location.toLowerCase() > b.location.toLowerCase()) return 1;
+            return lessons.sort((a, b) => {
+              if (a.location.toLowerCase() > b.location.toLowerCase())
+                return 1;
               if (a.location.toLowerCase() < b.location.toLowerCase())
                 return -1;
               return 0;
             });
-          case "Price":
-            return subjects.sort((a, b) => a.Price - b.Price);
-          case "availableInventory":
-            return subjects.sort(
-              (a, b) => a.availableInventory - b.availableInventory
-            );
-          default:
-            return subjects;
+          case "price":
+            return lessons.sort((a, b) => {
+              if (a.price > b.price) return 1;
+              if (a.price < b.price) return -1;
+              return 0;
+            });
+          case "availability":
+            return lessons.sort((a, b) => {
+              if (a.spaces > b.spaces) return 1;
+              if (a.spaces < b.spaces) return -1;
+              return 0;
+            });
         }
-      } else if (this.sortOrder === "desc") {
-        // Descending order
-        switch (this.sortAttribute) {
-          case "name":
-            return subjects.sort((a, b) => {
-              if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
-              if (a.name.toLowerCase() < b.name.toLowerCase()) return 1;
+      } else if (this.orderBy === "desc") {
+        // descending
+        switch (this.sortBy) {
+          case "subject":
+            return lessons.sort((a, b) => {
+              if (a.subject.toLowerCase() > b.subject.toLowerCase())
+                return -1;
+              if (a.subject.toLowerCase() < b.subject.toLowerCase())
+                return 1;
               return 0;
             });
           case "location":
-            return subjects.sort((a, b) => {
+            return lessons.sort((a, b) => {
               if (a.location.toLowerCase() > b.location.toLowerCase())
                 return -1;
-              if (a.location.toLowerCase() < b.location.toLowerCase()) return 1;
+              if (a.location.toLowerCase() < b.location.toLowerCase())
+                return 1;
               return 0;
             });
           case "price":
-            return subjects.sort((a, b) => b.Price - a.Price);
-          case "availableInventory":
-            return subjects.sort(
-              (a, b) => b.availableInventory - a.availableInventory
-            );
-          default:
-            return subjects;
+            return lessons.sort((a, b) => {
+              if (a.price > b.price) return -1;
+              if (a.price < b.price) return 1;
+              return 0;
+            });
+          case "availability":
+            return lessons.sort((a, b) => {
+              if (a.spaces > b.spaces) return -1;
+              if (a.spaces < b.spaces) return 1;
+              return 0;
+            });
         }
       }
     },
-
-
-
-    // cartSubjects: function () {
-    //   return this.cart.map((itemId) => {
-    //     const subject = this.subject.find((subject) => subject.id === itemId);
-    //     return {
-    //       ...subject,
-    //       spaces: subject.availableInventory - this.cartCount(subject.id),
-    //     };
-    //   });
-    // },
-
-
-    cartSubjects: function () {
-      const selectedCounts = {};
-
-      // Count the occurrences of each subject in the cart
-      this.cart.forEach((itemId) => {
-        selectedCounts[itemId] = (selectedCounts[itemId] || 0) + 1;
-      });
-
-      // Create an array of unique subjects with their counts
-      const uniqueSubjects = [...new Set(this.cart)];
-
-      // Map the unique subjects to include count information
-      return uniqueSubjects.map((itemId) => {
-        const subject = this.subject.find((subject) => subject.id === itemId);
-        const selectedCount = selectedCounts[itemId] || 0;
-        return {
-          ...subject,
-          spaces: subject.availableInventory - this.cartCount(subject.id),
-          selectedCount: selectedCount,
-        };
-      });
+    isCheckoutFormValid() {
+      return Object.keys(this.checkoutForm).every(
+        (key) =>
+          this.checkoutForm[key].value && !this.checkoutForm[key].error
+      );
     },
-
   },
 
-  created(){
+  created() {
     this.getLessons();
- },
+  },
 
- watch: {
-   searchText: {
-     handler(val){
-       this.getLessons();
-     }
-   }
- },
+  watch: {
+    searchText: {
+      handler(val) {
+        this.getLessons();
+      },
+    },
+    "checkoutForm.name": {
+      handler(val) {
+        const validationRegex = /^[A-Za-z\s]*$/;
+
+        if (!val.value) val.error = "Please enter your name";
+        else if (!validationRegex.test(val.value))
+          val.error = "Your name must only contain letters";
+        else val.error = "";
+      },
+      deep: true,
+    },
+
+    "checkoutForm.phone": {
+      handler(val) {
+        const validationRegex = /^((\+44)|(0)) ?\d{4} ?\d{6}$/;
+
+        if (!val.value) val.error = "Please enter your phone number";
+        else if (!validationRegex.test(val.value))
+          val.error = "Please enter a valid UK phone number";
+        else val.error = "";
+      },
+      deep: true,
+    },
+  },
 });
