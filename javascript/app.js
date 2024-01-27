65e08b4174b2fc5933ce10bdb9a180c4477794e0
bhavneetkaurraheja <banirahejax@gmail.com>
2024-01-27 21:45:25 +0000

    updated

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,7 @@ new Vue({
   data: {
     loading: false,
     error: null,
-    url: "https://coursework2.eu-north-1.elasticbeanstalk.com",
+    url: "https://webcoursework2.eu-north-1.elasticbeanstalk.com",
     searchText: "",
     sortBy: "subject",
     orderBy: "asc",
@@ -176,6 +176,7 @@ new Vue({
 
       setTimeout(() => {
         this.checkedOut = false;
+        
       }, 3000);
     },
   },
