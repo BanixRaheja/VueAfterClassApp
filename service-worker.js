95f4167bc0390626bad29c302a398b2d4f4969a2
bhavneetkaurraheja <banirahejax@gmail.com>
2024-03-08 23:59:21 +0000

    updated

--- service-worker.js
+++ service-worker.js
@@ -19,37 +19,35 @@ var cacheFiles = [
     "lessons.js"
 ];
 
-self.addEventListener("install", function(e) {
+self.addEventListener("install", function (e) {
     console.log("[Service Worker] Install");
     e.waitUntil(
-        caches.open(cacheName). then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log("[Service Worker] Caching files");
             return cache.addAll(cacheFiles);
+        }).catch(function (error) {
+            console.error("[Service Worker] Error during installation:", error);
         })
     );
 });
 
-self.addEventListener("fetch", function (e){
+self.addEventListener("fetch", function (e) {
     e.respondWith(
         caches.match(e.request).then(function (cachedFile) {
-            // if the file is in cache, retrive it from there
-            if(cachedFile){
+            if (cachedFile) {
                 console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
                 return cachedFile;
-            } else //if the file is not in the cache, download the file
-            {
+            } else {
                 return fetch(e.request).then(function (response) {
                     return caches.open(cacheName).then(function (cache) {
-                        //add new file to the cache
                         cache.put(e.request, response.clone());
-
-                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
-                        e.request.url);
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " + e.request.url);
                         return response;
                     });
+                }).catch(function (error) {
+                    console.error("[Service Worker] Error during fetch:", error);
                 });
             }
         })
     );
 });
-
